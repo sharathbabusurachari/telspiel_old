92520ee6661f8496d3a9dde6b5b582e3a037e528
sharathbabusurachari <122995305+sharathbabusurachari@users.noreply.github.com>
2024-02-05 16:26:36 +0530

    Update TelspielApplication.java

--- src/main/java/com/saswath/telspiel/TelspielApplication.java
+++ src/main/java/com/saswath/telspiel/TelspielApplication.java
@@ -7,8 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.saswath.telspiel"})
-public class TelspielApplication {
+public class TelspielApplication extends SpringBootServletInitializer {
 
+	@RequestMapping(value= "/hello")
 	public static void main(String[] args) {
 		SpringApplication.run(TelspielApplication.class, args);
 	}
