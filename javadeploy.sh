86ecb20859a4ba04db7e630b3001d8e5da095d6b
sharathbabusurachari <122995305+sharathbabusurachari@users.noreply.github.com>
2024-02-06 18:21:48 +0530

    Update javadeploy.sh

--- javadeploy.sh
+++ javadeploy.sh
@@ -6,8 +6,8 @@ if [ -z "$status" ];
         then
         echo "Application is NOT running & we're starting now";
         sleep 30
-        chmod 755 $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.jar
-        java -jar $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.jar &
+        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar
+        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &
         if [[ "$?" == 0 ]]; then echo "Application has been started successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`; exit 0; 
         else echo "Application deployment failed"; exit 1; fi
 
@@ -18,8 +18,8 @@ else
                 else echo "Failed to stop previous instance of the Application"; exit 1;
         fi
         sleep 30
-        chmod 755 $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.jar
-        java -jar $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.jar &
+        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar
+        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &
         if [[ "$?" == 0 ]]; then echo "Application has been deployed successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`; exit 0; 
         else echo "Application deployment failed"; exit 1; fi
 fi
