567b29619f0442844a3b94065612aa297b84d593
sharathbabusurachari <122995305+sharathbabusurachari@users.noreply.github.com>
2024-02-06 20:43:47 +0530

    Update javadeploy.sh

--- javadeploy.sh
+++ javadeploy.sh
@@ -5,9 +5,10 @@ status=`ps -ef | grep telspiel | grep jar | awk '{print $2}'`
 if [ -z "$status" ];
         then
         echo "Application is NOT running & we're starting now...";
-        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar
-        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &
-        sleep 30
+        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar;
+        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &;
+        sleep 30;
+        echo "Done..";
         if [[ "$?" == 0 ]]; then echo "Application has been started successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`; exit 0; 
         else echo "Application deployment failed"; exit 1; fi
 
@@ -17,9 +18,10 @@ else
         if [[ "$?" == 0 ]]; then echo "Previous instance of the Application has been stopped successfully";
                 else echo "Failed to stop previous instance of the Application"; exit 1;
         fi
-        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar
-        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &
-        sleep 30
+        chmod 755 /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar;
+        java -jar /var/lib/jenkins/workspace/01_Java_CICD/target/telspiel-0.0.1-SNAPSHOT.jar &;
+        sleep 30;
+        echo "Done..";
         if [[ "$?" == 0 ]]; then echo "Application has been deployed successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`; exit 0; 
         else echo "Application deployment failed"; exit 1; fi
 fi
